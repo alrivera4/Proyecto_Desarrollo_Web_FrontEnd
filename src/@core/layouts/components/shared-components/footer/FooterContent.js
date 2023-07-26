198d481d17ad06f8154c8531242c151e1198c459
alrivera4 <alrivera4@espe.edu.ec>
2023-07-26 18:32:33 -0500

    footer

--- src/@core/layouts/components/shared-components/footer/FooterContent.js
+++ src/@core/layouts/components/shared-components/footer/FooterContent.js
@@ -11,13 +11,13 @@ const FooterContent = () => {
   return (
     <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
       <Typography sx={{ mr: 2 }}>
-        {`© ${new Date().getFullYear()}, Made with `}
+        {` `}
         <Box component='span' sx={{ color: 'error.main' }}>
-          ❤️
+
         </Box>
-        {` by `}
+        {`  `}
         <Link target='_blank' href='https://themeselection.com/'>
-          ThemeSelection
+
         </Link>
       </Typography>
       {hidden ? null : (
@@ -26,22 +26,22 @@ const FooterContent = () => {
             target='_blank'
             href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/LICENSE'
           >
-            MIT License
+
           </Link>
           <Link target='_blank' href='https://themeselection.com/'>
-            More Themes
+
           </Link>
           <Link
             target='_blank'
             href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/README.md'
           >
-            Documentation
+
           </Link>
           <Link
             target='_blank'
             href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/issues'
           >
-            Support
+
           </Link>
         </Box>
       )}
