178dd8a6e839317fbbecb01bac3dda26c3be32fb
Samantha Nicole Tipanguano Santamaria <75260906+sntipanguano@users.noreply.github.com>
2023-07-27 15:49:20 -0500

    Form Trayectoria

--- src/views/form-layouts/FormLayoutsTrayectoria.js
+++ src/views/form-layouts/FormLayoutsTrayectoria.js
@@ -22,80 +22,148 @@ import CloudUploadIcon from '@mui/icons-material/CloudUpload'
 // ** Third Party Imports
 import MessageOutline from 'mdi-material-ui/MessageOutline'
 
+// Helper function to validate only letters and spaces
+
+
 const FormLayoutsTrayectoria = () => {
   // ** States
   const [file, setFile] = useState(null);
-  const [title, setTitle] = useState(''); // Add the state for title
-  const [titleError, setTitleError] = useState('');
-  const [institution, setInstitution] = useState(''); // Add the state for institution
+  const [institution, setInstitution] = useState('');
   const [institutionError, setInstitutionError] = useState('');
-  const [selectedYear, setSelectedYear] = useState('');
-  const [descripcion, setDescripcion] = useState('');
-  const [descripcionError, setDescripcionError] = useState('');
+  const [department, setDepartment] = useState('');
+  const [departmentError, setDepartmentError] = useState('');
+  const [position, setPosition] = useState('');
+  const [positionError, setPositionError] = useState('');
+  const [contractType, setContractType] = useState('');
+  const [contractTypeError, setContractTypeError] = useState('');
+  const [institutionType, setInstitutionType] = useState('');
+  const [institutionTypeError, setInstitutionTypeError] = useState('');
+  const [experienceType, setExperienceType] = useState('');
+  const [experienceTypeError, setExperienceTypeError] = useState('');
+  const [contractStart, setContractStart] = useState('');
+  const [contractStartError, setContractStartError] = useState('');
+  const [contractEnd, setContractEnd] = useState('');
+  const [contractEndError, setContractEndError] = useState('');
+
+
+  const isLettersAndSpacesOnly = (value) => /^[a-zA-Z\s]*$/.test(value);
+
+  const isValidDate = (value) => {
+    return !isNaN(Date.parse(value));
+  };
+
 
   // Handle File Upload
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
 
-  const handleYearChange = (event) => {
-    setSelectedYear(event.target.value);
-  };
-
-  const [selectedValues, setSelectedValues] = useState({
-    areaEstudio: '',
-    nivelEstudio: '',
-    estadoEstudio: '',
-  });
-
   // Handle Form Submission
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const regex = /^[a-zA-Z\s]*$/;
-    
-    if (!regex.test(title)) {
-      setTitleError('Title should only contain letters and spaces');
-    } else {
-      setTitleError('');
-    }
+      let formIsValid = true;
+  
+      // Validate "Institución"
+      if (!isLettersAndSpacesOnly(institution)) {
+        setInstitutionError('Institution should only contain letters and spaces');
+        formIsValid = false;
+      } else {
+        setInstitutionError('');
+      }
+  
+      // Validate "Departamento"
+      if (!isLettersAndSpacesOnly(department)) {
+        setDepartmentError('Departamento should only contain letters and spaces');
+        formIsValid = false;
+      } else {
+        setDepartmentError('');
+      }
+  
+      // Validate "Cargo"
+      if (!isLettersAndSpacesOnly(position)) {
+        setPositionError('Cargo should only contain letters and spaces');
+        formIsValid = false;
+      } else {
+        setPositionError('');
+      }
+  
+      // Validate "Tipo de Contrato"
+      if (!contractType) {
+        // Assuming that the initial value of contractType is an empty string
+        // Change this if the initial value is different
+        setContractTypeError('Please select a Tipo de Contrato');
+        formIsValid = false;
+      } else {
+        setContractTypeError('');
+      }
+  
+      // Validate "Tipo de Institución"
+      if (!institutionType) {
+        // Assuming that the initial value of institutionType is an empty string
+        // Change this if the initial value is different
+        setInstitutionTypeError('Please select a Tipo de Institución');
+        formIsValid = false;
+      } else {
+        setInstitutionTypeError('');
+      }
+  
+      // Validate "Tipo de Experiencia"
+      if (!experienceType) {
+        // Assuming that the initial value of experienceType is an empty string
+        // Change this if the initial value is different
+        setExperienceTypeError('Please select a Tipo de Experiencia');
+        formIsValid = false;
+      } else {
+        setExperienceTypeError('');
+      }
+  
+      // Validate "Inicio Contrato" date format
+      if (!contractStart) {
+        setContractStartError('Please enter the start date');
+        formIsValid = false;
+      } else if (!isValidDate(contractStart)) {
+        setContractStartError('Please enter a valid date (YYYY-MM-DD)');
+        formIsValid = false;
+      } else {
+        setContractStartError('');
+      }
 
-    if (!regex.test(institution)) {
-      setInstitutionError('Institution should only contain letters and spaces');
-    } else {
-      setInstitutionError('');
-    }
+  
+      // Validate "Fin Contrato" date format
+      if (!contractEnd) {
+        setContractEndError('Please enter the end date');
+        formIsValid = false;
+      } else if (!isValidDate(contractEnd)) {
+        setContractEndError('Please enter a valid date (YYYY-MM-DD)');
+        formIsValid = false;
+      } else {
+        setContractEndError('');
+      }
+  
+      // Continue with form submission if all fields are valid
+      if (formIsValid && contractStart > contractEnd) {
+        setContractEndError('The end date should be after the start date');
+        formIsValid = false;
+      }
 
-    if (!regex.test(descripcion)) {
-      setDescripcionError('Description should only contain letters and spaces');
-    } else {
-      setDescripcionError('');
-    }
+      if (formIsValid) {
+        // Perform the form submission logic here
+        console.log('Form submitted successfully');
 
-    // Continue with form submission if all fields are valid
-    if (regex.test(title) && regex.test(institution) && regex.test(descripcion)) {
-      // Perform the form submission logic here
-      console.log('Form submitted successfully');
 
-      // Clear the form data after successful submission
-      setTitle('');
-      setInstitution('');
-      setFile(null);
-      setSelectedYear('');
-      setDescripcion('');
-
-      setSelectedValues({
-        areaEstudio: '',
-        nivelEstudio: '',
-        estadoEstudio: '',
-      })
-      }   
+        // Clear the form data after successful submission
+        setInstitution('');
+        setDepartment('');
+        setPosition('');
+        setFile(null);
+        setContractType('');
+        setInstitutionType('');
+        setExperienceType('');
+        setContractStart('');
+        setContractEnd('');
+      }
     };
-
-  const startYear = 1980;
-  const currentYear = new Date().getFullYear();
-  const years = Array.from({ length: currentYear - startYear + 1 }, (_, index) => startYear + index);
-
 
   return (
     <Card>
@@ -106,164 +174,121 @@ const FormLayoutsTrayectoria = () => {
         <Grid item xs={12}>
         </Grid>
         <Grid item xs={12} sm={6}>
-        <Typography variant='body2' sx={{ fontWeight: 600 }}>Título</Typography>
+        <Typography variant='body2' sx={{ fontWeight: 600 }}>Institución</Typography>
           <TextField 
             fullWidth 
             placeholder=''
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            error={!!titleError}
-            helperText={titleError}
-            required 
+            value={institution}
+            onChange={(e) => setInstitution(e.target.value)}
+            error={!!institutionError}
+            helperText={institutionError}
+            required  
           />
         </Grid>
         <Grid item xs={12} sm={6}>
-        <Typography variant='body2' sx={{ fontWeight: 600 }}>Institución</Typography>
+        <Typography variant='body2' sx={{ fontWeight: 600 }}>Departamento</Typography>
             <TextField
              fullWidth 
              placeholder='' 
-             value={institution}
-             onChange={(e) => setInstitution(e.target.value)}
-             error={!!institutionError}
-             helperText={institutionError}
+             value={department}
+             onChange={(e) => setDepartment(e.target.value)}
+             error={!!departmentError}
+             helperText={departmentError}
              required
             />
         </Grid>
         <Grid item xs={12} sm={6}>
+        <Typography variant='body2' sx={{ fontWeight: 600 }}>Cargo</Typography>
+            <TextField
+              fullWidth 
+              placeholder='' 
+              value={position}
+              onChange={(e) => setPosition(e.target.value)}
+              error={!!positionError}
+              helperText={positionError}
+              required
+            />
+        </Grid>
+        <Grid item xs={12} sm={6}>
           <FormControl fullWidth>
-          <Typography variant='body2' sx={{ fontWeight: 600 }}>Areá de Estudio</Typography>
+          <Typography variant='body2' sx={{ fontWeight: 600 }}>Tipo de Contrato</Typography>
           <InputLabel id='form-layouts-separator-multiple-select-label-2'></InputLabel>
             <Select
-              id='form-layouts-separator-select'
-              labelId='form-layouts-separator-select-label'
-              label='Country'
-              placeholder='Bio...'
-              MenuProps={{
-                PaperProps: {
-                  style: {
-                    maxHeight: 200, // Ajusta esta altura según tus necesidades
-                  },
-                }
-              }}
-              value={selectedValues.areaEstudio}
-              onChange={(e) =>
-                setSelectedValues({
-                  ...selectedValues,
-                  areaEstudio: e.target.value,
-                })
-              }
+            labelId='contract-type-label'
+            label='Tipo de Contrato'
+            value={contractType}
+            onChange={(e) => setContractType(e.target.value)}
+            error={!!contractTypeError}
             >
-              <MenuItem value='CS'>Ciencias Sociales</MenuItem>
-              <MenuItem value='CN'>Ciencias Naturales</MenuItem>
-              <MenuItem value='HU'>Humanidades</MenuItem>
-              <MenuItem value='CSalud'>Ciencias de la Salud</MenuItem>
-              <MenuItem value='IT'>Ingeniría y Tecnología</MenuItem>
-              <MenuItem value='Artes'>Artes y Bellas Artes</MenuItem>
-              <MenuItem value='CTierra'>Ciencias de la Tierra y Medio Ambiente</MenuItem>
-              <MenuItem value='Bio'>Biotecnología y Genética</MenuItem>
+              <MenuItem value='Contrato1'>Contrato 1</MenuItem>
+              <MenuItem value='Contrato2'>Contrato 2</MenuItem>
             </Select>
+            {contractTypeError && <Typography variant='body2' color='error'>{contractTypeError}</Typography>}
           </FormControl>
         </Grid>
         <Grid item xs={12} sm={6}>
           <FormControl fullWidth>
-          <Typography variant='body2' sx={{ fontWeight: 600 }}>Nivel de Estudio</Typography>
+          <Typography variant='body2' sx={{ fontWeight: 600 }}>Tipo de Institución</Typography>
           <InputLabel id='form-layouts-separator-multiple-select-label-2'></InputLabel>
             <Select
-              id='form-layouts-separator-select'
-              labelId='form-layouts-separator-select-label'
-              label='Country'
-              placeholder='Bio...'
-              MenuProps={{
-                PaperProps: {
-                  style: {
-                    maxHeight: 200, // Ajusta esta altura según tus necesidades
-                  },
-                }
-              }}
-              value={selectedValues.nivelEstudio}
-              onChange={(e) =>
-                setSelectedValues({
-                  ...selectedValues,
-                  nivelEstudio: e.target.value,
-                })
-              }
+            labelId='contract-type-label'
+            label='Tipo de Contrato'
+            value={institutionType}
+            onChange={(e) => setInstitutionType(e.target.value)}
+            error={!!institutionTypeError}
             >
-              <MenuItem value='Pre'>Pregrado</MenuItem>
-              <MenuItem value='Pos'>Posgrado</MenuItem>
-              <MenuItem value='Lic'>Licenciatura</MenuItem>
-              <MenuItem value='Tec'>Tecnología</MenuItem>
-              <MenuItem value='Doc'>Doctorado</MenuItem>
+              <MenuItem value='Contrato1'>Contrato 1</MenuItem>
+              <MenuItem value='Contrato2'>Contrato 2</MenuItem>
             </Select>
+            {institutionTypeError && <Typography variant='body2' color='error'>{institutionTypeError}</Typography>}
           </FormControl>
         </Grid>
         <Grid item xs={12} sm={6}>
           <FormControl fullWidth>
-          <Typography variant='body2' sx={{ fontWeight: 600 }}>Estado de Estudio</Typography>
+          <Typography variant='body2' sx={{ fontWeight: 600 }}>Tipo de Experiencia</Typography>
+          <InputLabel id='form-layouts-separator-multiple-select-label-2'></InputLabel>
+            <Select
+              labelId='experience-type-label'
+              label='Tipo de Experiencia'
+              value={experienceType}
+              onChange={(e) => setExperienceType(e.target.value)}
+              error={!!experienceTypeError}
+            >
+              <MenuItem value='Experiencia1'>Experiencia 1</MenuItem>
+              <MenuItem value='Experiencia2'>Experiencia 2</MenuItem>
+            </Select>
+            {experienceTypeError && <Typography variant='body2' color='error'>{experienceTypeError}</Typography>}
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+          <Typography variant='body2' sx={{ fontWeight: 600 }}>Inicio Contrato</Typography>
             <InputLabel id='form-layouts-separator-select-label-2'></InputLabel>
-            <Select
-              label='Country 2'
-              defaultValue=''
-              id='form-layouts-separator-select-2'
-              labelId='form-layouts-separator-select-label-2'
-              MenuProps={{
-                PaperProps: {
-                  style: {
-                    maxHeight: 200, // Ajusta esta altura según tus necesidades
-                  },
-                }
-              }}
-              value={selectedValues.estadoEstudio}
-              onChange={(e) =>
-                setSelectedValues({
-                  ...selectedValues,
-                  estadoEstudio: e.target.value,
-                })
-              }
-            >
-              <MenuItem value='Parcial'>Estudiante de Tiempo Parcial</MenuItem>
-              <MenuItem value='Graduado'>Graduado</MenuItem>
-              <MenuItem value='Practicas'>Estudiante de Prácticas</MenuItem>
-            </Select>
+            <TextField
+              fullWidth
+              type='date'
+              value={contractStart}
+              onChange={(e) => setContractStart(e.target.value)}
+              error={!!contractStartError}
+              helperText={contractStartError}
+            />
           </FormControl>
         </Grid>
         <Grid item xs={12} sm={6}>
           <FormControl fullWidth>
-          <Typography variant='body2' sx={{ fontWeight: 600 }}>Año de Inicio</Typography>
+          <Typography variant='body2' sx={{ fontWeight: 600 }}>Fin Contrato</Typography>
           <InputLabel id='form-layouts-separator-multiple-select-label-2'></InputLabel>
-          <Select
-          labelId='form-layouts-separator-select-label'
-          label='Año de Inicio'
-          value={selectedYear}
-          onChange={handleYearChange}
-        >
-          {years.map((year) => (
-            <MenuItem key={year} value={year}>{year}</MenuItem>
-          ))}
-          </Select>
+          <TextField
+              fullWidth
+              type='date'
+              value={contractEnd}
+              onChange={(e) => setContractEnd(e.target.value)}
+              error={!!contractEndError}
+              helperText={contractEndError}
+          />
           </FormControl>
         </Grid>
       </Grid>  
-      <br></br>
-      <Grid item xs={12}>
-      <Typography variant='body2' sx={{ fontWeight: 600 }}>Descripción</Typography>
-              <TextField
-                fullWidth
-                multiline
-                minRows={3}
-                label=''
-                placeholder='Bio...'
-                value={descripcion}
-                onChange={(e) => setDescripcion(e.target.value)}
-                sx={{ '& .MuiOutlinedInput-root': { alignItems: 'baseline' } }}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position='start'>
-                      <MessageOutline />
-                    </InputAdornment>
-                  )
-                }}
-              />
-            </Grid>
       <br></br>
       <Divider sx={{ margin: 0 }} />
       <br></br>
