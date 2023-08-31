a7f24cab4f53b245f193e81bbbc11d9b894a35b1
Sebastian David Falconi Davalos <sdfalconi@espe.edu.ec>
2023-08-31 17:54:07 -0500

    v166

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,84 @@ const express = require('express')
 const { body, validationResult } = require('express-validator')
 const bcrypt = require('bcrypt')
 const { generateToken, authenticateToken } = require('../authUtils.js')
+const { Sequelize, DataTypes } = require('sequelize')
 const jwt = require('jsonwebtoken')
-const { Usuario} = require('../index.js')
+const { Client } = require('pg');
+
+
 const router = express.Router()
+
+// Conexión a PostgreSQL sin especificar una base de datos
+const sequelize = new Sequelize('postgres', 'postgres', '123123', {
+  host: 'postgresql_container', // Cambia esto a tu host de PostgreSQL
+  port: 5432,
+  dialect: 'postgres',
+  define: {
+    timestamps: false, // Deshabilitar los campos createdAt y updatedAt
+  },
+});
+
+// Crear la base de datos
+const client = new Client({
+  user: 'postgres',
+  host: 'postgresql_container', // Cambia esto a tu host de PostgreSQL
+  database: 'postgres',
+  password: '123123',
+  port: 5432,
+});
+
+client.connect()
+  .then(() => {
+    return client.query('CREATE DATABASE proyectopost');
+  })
+  .then(() => {
+    client.end(); // Cerrar la conexión
+
+    // Conexión a la base de datos recién creada
+    const db = new Sequelize('proyectopost', 'postgres', '123123', {
+      host: 'postgresql_container',
+      port: 5432,
+      dialect: 'postgres',
+      define: {
+        timestamps: false,
+      },
+    });
+
+    // Definición del modelo de Usuario
+    const Usuario = db.define('usuarios', {
+      numeroidentificacion: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+      },
+      tipoidentificacion: {
+        type: DataTypes.STRING,
+      },
+      correo: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      nombres: {
+        type: DataTypes.STRING,
+      },
+      apellidos: {
+        type: DataTypes.STRING,
+      },
+    });
+
+    // Sincronizar el modelo con la base de datos
+    db.sync()
+      .then(() => {
+        console.log('Base de datos y tabla creadas exitosamente.');
+      })
+      .catch((error) => {
+        console.error('Error al crear la base de datos y tabla:', error);
+      });
+  })
+  .catch((error) => {
+    console.error('Error al crear la base de datos:', error);
+  });
 
 // Ruta de registro de usuarios
 router.post(
